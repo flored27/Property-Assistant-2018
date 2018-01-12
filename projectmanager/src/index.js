733418d5ddcea017ee904e3aec0ce86b1ef024aa
flored27 <dflores@fandm.edu>
2018-01-12 14:31:39 -0500

    copy/pasted index.js *not sure about registerServiceWorker

--- projectmanager/src/index.js
+++ projectmanager/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+const initialState = { currentUser: {} };
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_CURRENT_USER':
+      const { id, username } = action.user;
+      return { ...state, currentUser: { id, username } };
+    case 'LOGOUT_USER':
+      return { ...state, currentUser: {} };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+const Root = ({ store }) => {
+  return (
+    <Router>
+      <Provider store={store}>
+        <Route path="/" component={App} />
+      </Provider>
+    </Router>
+  );
+};
+
+ReactDOM.render(<Root store={store} />, document.getElementById('root'));
 registerServiceWorker();
