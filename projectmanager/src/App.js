b43cd4d5db439982ceb45b16fe47d8e14c92f278
flored27 <dflores@fandm.edu>
2018-01-12 09:35:17 -0500

    added redux and redux devtools

--- projectmanager/src/App.js
+++ projectmanager/src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Icon } from 'semantic-ui-react'
+
+
 
 class App extends Component {
+
+
   render() {
+
+    const ButtonExampleLabeledIcon = () => (
+      <div>
+        <Button icon labelPosition='left'>
+          <Icon name='pause' />
+          Pause
+        </Button>
+        <Button icon labelPosition='right'>
+          Next
+          <Icon name='right arrow' />
+        </Button>
+      </div>
+    )
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ButtonExampleLabeledIcon />
       </div>
     );
   }
