9278386b5d4afd238dcc5e403643864efbeea93a
flored27 <dflores@fandm.edu>
2018-01-16 09:39:40 -0500

    deleted console.log "inside set_current user" & "inside login_user"

--- projectmanager/src/reducers/AuthReducer.js
+++ projectmanager/src/reducers/AuthReducer.js
@@ -2,10 +2,8 @@
 export const authReducer = (state = {currentUser: {}}, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
-        console.log("inside set_current_user")
       return { ...state, currentUser: {id: action.payload.id, email: action.payload.email, properties: action.payload.properties}};
     case 'LOGIN_USER':
-      console.log("inside login_user")
       return {...state, currentUser: {id: action.payload.id, email: action.payload.email, properties: action.payload.properties}}
     case 'LOGOUT_USER':
       return { ...state, currentUser: {} };
