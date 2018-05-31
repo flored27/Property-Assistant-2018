654f740a60871c5a8b2e8d0a089b2d18d0361063
flored27 <dflores@fandm.edu>
2018-05-31 13:33:05 -0400

    deleted semantic-ui import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
