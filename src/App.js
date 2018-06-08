a5c1092dcf72aaad80610c5ddf4ee188caa62f03
flored27 <dflores@fandm.edu>
2018-06-08 09:06:01 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route, Router } from 'react-router-dom';
-import history from './history'
 import { connect } from 'react-redux';
+import { Link, Switch, Route, Router } from 'react-router-dom';
+import history from './history';
+import * as actions from './actions';
+import Initial from './Initial';
 import Login from './Login';
 import Profile from './Profile';
 import Property from './Property';
-import * as actions from './actions';
 import PropertyDetail from './PropertyDetail';
 import NewProperty from './NewProperty';
 import Register from './Register';
@@ -16,7 +17,6 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 import TextField from 'material-ui/TextField';
-import Initial from './Initial';
 
 class App extends Component {
   constructor(props){
@@ -35,146 +35,124 @@ class App extends Component {
   handleClick = (event) =>{
     event.preventDefault();
     console.log(this.state)
-   this.props.loginUser(this.state.email, this.state.password).then(()=>history.push("/profile"));
-   this.setState({
+    this.props.loginUser(this.state.email, this.state.password).then(()=>history.push("/profile"));
+    this.setState({
      loginPage: []
-   })
- }
+    })
+  }
 
   registerLink = (e)=>{
      e.preventDefault();
-     window.location = '/register';}
+     window.location = '/register';
+  }
      // This is a reference to the path of register:
      // //register, so that the Login/Register onclick goes to register,
      // and it works with githubpages due to the need of  in front of path
 
   handleOpen = () => {
-     this.setState({open: true});
-     };
+    this.setState({open: true});
+  };
 
   handleClose = () => {
-     this.setState({open: false});
-     };
-
-  componentWillMount(){
-    var loginPage =[];
-    loginPage.push(<Loginscreen parentContext={this}/>);
-    this.setState({
-                  loginPage:loginPage
-                    })
-  }
+    this.setState({open: false});
+  };
 
   render() {
-
     const actions = [
-        <FlatButton
-          label="Cancel"
-          primary={true}
-          onClick={this.handleClose}
-        />,
-        ];
-
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onClick={this.handleClose}
+      />
+    ];
     return (
       <Router history={history}>
-      <div className="App">
-      <MuiThemeProvider>
-        <div id="navbar" class="ui fixed sticky blue inverted menu">
-          <div class="item">
-          <img class="ui mini circular image" src="logo2.png"/>
+        <div className="App">
+          <MuiThemeProvider>
+            <div id="navbar" class="ui fixed sticky blue inverted menu">
+              <div class="item">
+                <img class="ui mini circular image" src="logo2.png"/>
                 <Link to="/">Home</Link>
-
-
-
-          </div>
-          <div class="item">
-            {this.props.loggedIn ? (
-                <Link to="/contact">Contact</Link>
-            ) : (
-              ""
-            )}
-          </div>
-          <div class="item">
-            {this.props.loggedIn ? (
-                <Link to="/properties">Properties</Link>
-            ) : (
-              ""
-            )}
-          </div>
-          <div class="right menu">
-          <div class="ui item">
-            {this.props.loggedIn ? (
-                <Link to="/profile">Profile</Link>
-            ) : (
-              ""
-            )}
-          </div>
-            <div class="ui item">
-            {this.props.loggedIn ? (
-              <div
-                onClick={e => {
-                  e.preventDefault();
-                  this.props.logoutUser();
-                }}
-              >
-                <FlatButton>
-                <Link to="/">Sign Out</Link>
-                </FlatButton>
               </div>
-            ) : (
-              <div>
-
-                <div>
-               <RaisedButton label="Login" onClick={this.handleOpen} />
-               <Dialog
-                 contentStyle={{maxWidth: 305}}
-                 title="Login"
-                 actions={actions}
-                 modal={true}
-                 open={this.state.open}
-               >
-               <TextField
-                 hintText="Enter your Email"
-                 floatingLabelText="Email"
-
-                 onChange = {(event,newValue) => this.setState({email:newValue})}
-                 />
-               <br/>
-                 <TextField
-                   type="password"
-                   hintText="Enter your Password"
-                   floatingLabelText="Password"
-                   onChange = {(event,newValue) => this.setState({password:newValue})}
-                   />
-                 <br/>
-                 <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-                 <RaisedButton label="Register" style={style} primary={true} onClick={this.registerLink}/>
-               </Dialog>
-             </div>
-
-          </div>
-            )}
+              <div class="item">
+                {this.props.loggedIn ? (
+                  <Link to="/contact">Contact</Link>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div class="item">
+                {this.props.loggedIn ? (
+                  <Link to="/properties">Properties</Link>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div class="right menu">
+                <div class="ui item">
+                  {this.props.loggedIn ? (
+                    <Link to="/profile">Profile</Link>
+                  ) : (
+                    ""
+                  )}
+                </div>
+                <div class="ui item">
+                  {this.props.loggedIn ? (
+                    <div
+                      onClick={e => {
+                      e.preventDefault();
+                      this.props.logoutUser();
+                      }}
+                    >
+                      <FlatButton>
+                        <Link to="/">Sign Out</Link>
+                      </FlatButton>
+                    </div>
+                  ) : (
+                    <div>
+                      <RaisedButton label="Login" onClick={this.handleOpen} />
+                      <Dialog
+                        contentStyle={{maxWidth: 305}}
+                        title="Login"
+                        actions={actions}
+                        modal={true}
+                        open={this.state.open}
+                      >
+                        <TextField
+                          hintText="Enter your Email"
+                          floatingLabelText="Email"
+                          onChange = {(event,newValue) => this.setState({email:newValue})}
+                        />
+                        <br/>
+                        <TextField
+                          type="password"
+                          hintText="Enter your Password"
+                          floatingLabelText="Password"
+                          onChange = {(event,newValue) => this.setState({password:newValue})}
+                        />
+                        <br/>
+                        <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+                        <RaisedButton label="Register" style={style} primary={true} onClick={this.registerLink}/>
+                      </Dialog>
+                    </div>
+                  )}
+                </div>
+              </div>
             </div>
-          </div>
+
+            <Switch>
+              <Route path="/" component={Initial} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/login" component={Login} />
+              <Route path="/properties" component={Property} />
+              <Route path="/detail/:id" render={(props)=><PropertyDetail{...props}/>} />
+              <Route path="/new_property" component={NewProperty} />
+              <Route path="/register" component={Register} />
+              <Route path="/contact" component={Message} />
+              <Route path="/edit_property/:id" render={(props)=><EditProperty{...props}/>}/>
+            </Switch>
+          </MuiThemeProvider>
         </div>
-
-
-
-        <Switch>
-          <Route path="/profile" component={Profile} />
-            <Route path="/loginscreen" component={Loginscreen} />
-            <Route path="/login" component={Login} />
-            <Route path="/properties" component={Property} />
-            <Route path="/detail/:id" render={(props)=><PropertyDetail{...props}/>} />
-            <Route path="/new_property" component={NewProperty} />
-            <Route path="/register" component={Register} />
-            <Route path="/contact" component={Message} />
-            <Route path="/edit_property/:id" render={(props)=><EditProperty{...props}/>}/>
-            <Route path="/" component={Initial} />
-
-          </Switch>
-</MuiThemeProvider>
-      </div>
-
       </Router>
     );
   }
@@ -182,7 +160,6 @@ class App extends Component {
 const style = {
   margin: 15,
 };
-
 
 const mapStateToProps = state => ({
   loggedIn: !!state.auth.currentUser.id
